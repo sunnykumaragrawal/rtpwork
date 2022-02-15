d9d5be4df3c5b99afa88b375084990ff550e0511
sunnykumaragrawal <sunny.agarwal865@gmail.com>
2022-02-15 19:03:51 +0530

    other version ready

---
+++ MDEN22/assets/js/main.js
@@ -0,0 +1,139 @@
+!(function($) {
+  "use strict";
+
+  // Toggle nav menu
+  $(document).on('click', '.nav-toggle', function(e) {
+    $('.nav-menu').toggleClass('nav-menu-active');
+    $('.nav-toggle').toggleClass('nav-toggle-active');
+    $('.nav-toggle i').toggleClass('bx-x bx-menu');
+
+  });
+
+  // Smooth scroll for the navigation menu and links with .scrollto classes
+  $(document).on('click', '.nav-menu a, .scrollto', function(e) {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      e.preventDefault();
+      var target = $(this.hash);
+      if (target.length) {
+
+        var scrollto = target.offset().top;
+
+        if ($(this).attr("href") == '#header') {
+          scrollto = 0;
+        }
+
+        $('html, body').animate({
+          scrollTop: scrollto
+        }, 1500, 'easeInOutExpo');
+
+        if ($(this).parents('.nav-menu').length) {
+          $('.nav-menu .active').removeClass('active');
+          $(this).closest('li').addClass('active');
+          $('.nav-menu').removeClass('nav-menu-active');
+          $('.nav-toggle').removeClass('nav-toggle-active');
+          $('.nav-toggle i').toggleClass('bx-x bx-menu');
+        }
+        return false;
+      }
+    }
+  });
+
+  // ========================================================================= //
+  //  Owl Carousel socialmedia
+  // ========================================================================= //
+
+  $('.recentblogs-carousel').owlCarousel({
+    autoplay: true,
+    loop: true,
+    margin: 20,
+    dots: false,
+    nav: true,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      768: {
+        items: 2
+      },
+      900: {
+        items: 2
+      }
+    }
+  });
+
+  // ========================================================================= //
+  //  Owl Carousel Insurnce
+  // ========================================================================= //
+
+  $('.insurance-carousel').owlCarousel({
+    autoplay: true,
+    loop: true,
+    margin: 30,
+    dots: false,
+    nav: false,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      768: {
+        items: 2
+      },
+      900: {
+        items: 4
+      }
+    }
+  });
+
+  // ========================================================================= //
+  //  Owl Carousel socialmedia
+  // ========================================================================= //
+
+  $('.socialmedia-carousel').owlCarousel({
+    autoplay: true,
+    loop: true,
+    margin: 20,
+    dots: false,
+    nav: true,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 3
+      },
+      768: {
+        items: 4
+      },
+      900: {
+        items: 4
+      }
+    }
+  });
+
+  // Aos animation
+  window.addEventListener('load', () => {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    })
+  });
+
+  // Back to top button
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('.back-to-top').fadeIn('slow');
+    } else {
+      $('.back-to-top').fadeOut('slow');
+    }
+  });
+
+  $('.back-to-top').click(function() {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1500, 'easeInOutExpo');
+    return false;
+  });
+
+})(jQuery);
