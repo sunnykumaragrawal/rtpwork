b91a26d036460e541c3ac9237a168e5a37ddd4bd
sunnykumaragrawal <sunny.agarwal865@gmail.com>
2022-07-11 19:20:43 +0530

    setup

--- MFAM29/assets/js/main.js
+++ MFAM29/assets/js/main.js
@@ -1,64 +1,60 @@
 !(function($) {
   "use strict";
 
-  // Preloader
-  $(window).on('load', function() {
-   $(".loader").fadeOut();
-   $("#preloder").delay(400).fadeOut("slow");	
+  // Toggle nav menu
+  $(document).on('click', '.nav-toggle', function(e) {
+    $('.image-animate').toggleClass('image-animate-active');
+    $('.nav-menu').toggleClass('nav-menu-active');
+    $('.nav-toggle').toggleClass('nav-toggle-active');
+    $('.nav-toggle i').toggleClass('bx-x bx-menu');
+
   });
 
-  // Init AOS
-  function aos_init() {
-    AOS.init({
-      duration: 1000,
-      once: true
-    });
-  }
-
-  //navbar
-  $(".menu-switch").on('click', function () {
-  	$('.side-menu-wrapper').addClass('active');
-  	$('.menu-wrapper').addClass('hide-left');
+  // Toggle .header-scrolled class to #header when page is scrolled
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 400) {
+      $('#header').addClass('header-scrolled');
+    } else {
+      $('#header').removeClass('header-scrolled');
+    }
   });
 
-  $(".menu-close").on('click', function () {
-  	$('.side-menu-wrapper').removeClass('active');
-  	$('.menu-wrapper').removeClass('hide-left');
+  // Smooth scroll for the navigation menu and links with .scrollto classes
+  $(document).on('click', '.nav-menu a, .scrollto', function(e) {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      e.preventDefault();
+      var target = $(this.hash);
+      if (target.length) {
+
+        var scrollto = target.offset().top;
+
+        if ($(this).attr("href") == '#header') {
+          scrollto = 0;
+        }
+
+        $('html, body').animate({
+          scrollTop: scrollto
+        }, 1500, 'easeInOutExpo');
+
+        if ($(this).parents('.nav-menu').length) {
+          $('.nav-menu .active').removeClass('active');
+          $(this).closest('li').addClass('active');
+          $('.image-animate').toggleClass('image-animate-active');
+          $('.nav-menu').removeClass('nav-menu-active');
+          $('.nav-toggle').removeClass('nav-toggle-active');
+          $('.nav-toggle i').toggleClass('bx-x bx-menu');
+        }
+        return false;
+      }
+    }
   });
-
-  function trainedPhy() {
-    var x = document.getElementById("about1");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
-
-  function extraCare() {
-    var x = document.getElementById("about2");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
-
-  function allSupport() {
-    var x = document.getElementById("about3");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
 
   // Hero carousel (uses the Owl Carousel library)
   $(".hero-carousel").owlCarousel({
     autoplay: true,
-    dots: true,
+    dots: false,
     loop: true,
-    margin:20,
+    margin:30,
     responsive: {
       0: {
         items: 1
@@ -91,25 +87,6 @@
     }
   });
 
-  // Medical Media carousel (uses the Owl Carousel library)
-  $(".medicalmedia-carousel").owlCarousel({
-    autoplay: true,
-    dots: false,
-    loop: true,
-    margin:30,
-    responsive: {
-      0: {
-        items: 3
-      },
-      768: {
-        items: 4
-      },
-      900: {
-        items: 6
-      }
-    }
-  });
-
   // Recentblogs carousel (uses the Owl Carousel library)
   $(".recentblogs-carousel").owlCarousel({
     autoplay: true,
@@ -121,12 +98,22 @@
         items: 1
       },
       768: {
-        items: 2
+        items: 1
       },
       900: {
-        items: 3
+        items: 1
       }
     }
+  });
+
+  // AOS animation
+  window.addEventListener('load', () => {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    })
   });
 
   // Back to top button
