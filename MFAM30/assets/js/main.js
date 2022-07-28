987321fd20f44fc8d79cde8564cfd810f2de4455
sunnykumaragrawal <sunny.agarwal865@gmail.com>
2022-07-28 18:54:03 +0530

    28-07-22

--- MFAM30/assets/js/main.js
+++ MFAM30/assets/js/main.js
@@ -1,6 +1,41 @@
 !(function($) {
   "use strict";
 
+  // Toggle .header-scrolled class to #header when page is scrolled
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 300) {
+      $('#header').addClass('header-scrolled');
+    } else {
+      $('#header').removeClass('header-scrolled');
+    }
+  });
+
+  // Smooth scroll for the navigation menu and links with .scrollto classes
+  $(document).on('click', '.overlay-content a, .scrollto', function(e) {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      e.preventDefault();
+      var target = $(this.hash);
+      if (target.length) {
+
+        var scrollto = target.offset().top;
+
+        if ($(this).attr("href") == '#header') {
+          scrollto = 0;
+        }
+
+        $('html, body').animate({
+          scrollTop: scrollto
+        }, 1500, 'easeInOutExpo');
+
+        if ($(this).parents('.overlay-content').length) {
+          $('#myNav').removeClass('menu_width');
+          $('.custom-btn').removeClass('custom-cross-btn');
+        }
+        return false;
+      }
+    }
+  });
+
   $("[title]").tooltip({
     delay: { "show": 500, "hide": 10 },
     offset: '0 10',
