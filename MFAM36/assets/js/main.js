a919857f634836a51823b1c35f4cf088a78f085a
sunnykumaragrawal <sunny.agarwal865@gmail.com>
2022-11-19 10:08:30 +0530

    project setup

---
+++ MFAM36/assets/js/main.js
@@ -0,0 +1,145 @@
+!(function($) {
+  "use strict";
+
+  // Preloader
+  $(window).on('load', function() {
+    if ($('#preloader').length) {
+      $('#preloader').delay(3000).fadeOut('slow', function() {
+        $(this).remove();
+      });
+    }
+  });
+
+  // AOS animation
+  window.addEventListener('load', () => {
+    AOS.init({
+      duration: 1200,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    })
+  });
+
+  // Back to top button
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('.back-to-top').addClass('showbacktop');
+    } else {
+      $('.back-to-top').removeClass('showbacktop');
+    }
+  });
+
+  $('.back-to-top').click(function() {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1500, 'easeInOutExpo');
+    return false;
+  });
+
+  // Toggle nav menu
+  $(document).on('click', '.nav-toggle', function(e) {
+    $('.nav-menu').toggleClass('nav-menu-active');
+    $('.nav-toggle').toggleClass('nav-toggle-active');
+    $('.nav-toggle i').toggleClass('bx-x bx-menu');
+
+  });
+
+  // Toogle nav menu drop-down items
+  $(document).on('click', '.nav-menu .drop-down > a', function(e) {
+    e.preventDefault();
+    $(this).next().slideToggle(300);
+    $(this).parent().toggleClass('active');
+  });
+
+  // Smooth scroll for the navigation menu and links with .scrollto classes
+  $(document).on('click', '.nav-menu a, .scrollto', function(e) {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      e.preventDefault();
+      var target = $(this.hash);
+      if (target.length) {
+
+        var scrollto = target.offset().top;
+
+        if ($(this).attr("href") == '#header') {
+          scrollto = 0;
+        }
+
+        $('html, body').animate({
+          scrollTop: scrollto
+        }, 1500, 'easeInOutExpo');
+
+        if ($(this).parents('.nav-menu').length) {
+          $('.nav-menu .active').removeClass('active');
+          $(this).closest('li').addClass('active');
+          $('.nav-menu').removeClass('nav-menu-active');
+          $('.nav-toggle').removeClass('nav-toggle-active');
+          $('.nav-toggle i').toggleClass('bx-x bx-menu');
+        }
+        return false;
+      }
+    }
+  });
+
+  // Social Media carousel (uses the Owl Carousel library)
+  $(".socialmedia-carousel").owlCarousel({
+    autoplay: true,
+    dots: false,
+    loop: true,
+    margin:30,
+    smartSpeed: 1200,
+    responsive: {
+      0: {
+        items: 3
+      },
+      768: {
+        items: 4
+      },
+      900: {
+        items: 5
+      }
+    }
+  });
+
+  // Resource carousel (uses the Owl Carousel library)
+  $(".resource-carousel").owlCarousel({
+    autoplay: true,
+    dots: false,
+    loop: true,
+    margin:30,
+    smartSpeed: 1200,
+    responsive: {
+      0: {
+        items: 1
+      },
+      768: {
+        items: 2
+      },
+      900: {
+        items: 4
+      }
+    }
+  });
+
+  // Testimonials carousel (uses the Owl Carousel library)
+  $('.testimonial-carousel').owlCarousel({
+    autoplay: true,
+    loop: true,
+    margin: 20,
+    dots: false,
+    nav: true,
+    smartSpeed: 1000,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      768: {
+        items: 1
+      },
+      900: {
+        items: 2
+      }
+    }
+  });
+
+})(jQuery);
